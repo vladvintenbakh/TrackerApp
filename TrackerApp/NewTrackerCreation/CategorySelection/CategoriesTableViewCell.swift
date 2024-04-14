8da6bd3e4afe79b33282d8a33dcf62eb02f976ae
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-04-14 13:46:18 +0800

    Added CategoriesTableViewCell

---
+++ TrackerApp/NewTrackerCreation/CategorySelection/CategoriesTableViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  CategoriesTableViewCell.swift
+//  TrackerApp
+//
+//  Created by Vlad Vintenbakh on 14/4/24.
+//
+
+import UIKit
+
+final class CategoriesTableViewCell: UITableViewCell {
+    static let identifier = "CategoriesTableViewCell"
+    
+    private let customSeparatorCell = CustomSeparatorCell()
+    
+    private let categoryNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        label.textColor = UIColor(named: "YPBlack")
+        return label
+    }()
+    
+    private let checkMarkImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "checkmark"))
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        
+        let views = [categoryNameLabel, checkMarkImageView]
+        views.forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(item)
+        }
+        
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            customSeparatorCell.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            customSeparatorCell.topAnchor.constraint(equalTo: contentView.topAnchor),
+            customSeparatorCell.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            customSeparatorCell.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            categoryNameLabel.leadingAnchor.constraint(equalTo: customSeparatorCell.leadingAnchor, constant: 16),
+            categoryNameLabel.trailingAnchor.constraint(equalTo: customSeparatorCell.trailingAnchor, constant: -41),
+            categoryNameLabel.centerYAnchor.constraint(equalTo: customSeparatorCell.centerYAnchor),
+            
+            checkMarkImageView.trailingAnchor.constraint(equalTo: customSeparatorCell.trailingAnchor, constant: -16),
+            checkMarkImageView.centerYAnchor.constraint(equalTo: categoryNameLabel.centerYAnchor)
+        ])
+    }
+    
+    func setLabelText(_ text: String) {
+        categoryNameLabel.text = text
+    }
+    
+    func showCheckMark(_ flag: Bool) {
+        checkMarkImageView.isHidden = !flag
+    }
+    
+    func configCustomCell(cellType: String) {
+        switch cellType {
+        case "First":
+            customSeparatorCell.setAsFirstCell()
+        case "Middle":
+            customSeparatorCell.setAsMiddleCell()
+        case "Last":
+            customSeparatorCell.setAsLastCell()
+        default:
+            return
+        }
+    }
+}

3e998256fba0be43220aa09d94b5ea953f9b4e69
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-04-14 13:31:35 +0800

    Fixed incorrect entry point

--- TrackerApp/AppDelegate.swift
+++ TrackerApp/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         window = UIWindow()
-        window?.rootViewController = CategorySelectionVC()
+        window?.rootViewController = EntryVC()
         window?.makeKeyAndVisible()
         return true
     }

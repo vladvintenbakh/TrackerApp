d21c92a6b453be0303bf20fbd29d1a65c8438053
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2024-04-25 17:00:16 +0800

    Added StatsVC layout

--- TrackerApp/StatsScreen/StatsViewController.swift
+++ TrackerApp/StatsScreen/StatsViewController.swift
@@ -8,8 +8,74 @@
 import UIKit
 
 final class StatsViewController: UIViewController {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = NSLocalizedString("statsScreen.title", comment: "")
+        label.textColor = UIColor(named: "YPBlack")
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        label.textAlignment = .left
+        
+        return label
+    }()
+    
+    private let placeholderImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "FaceWithTearPlaceholder"))
+        return imageView
+    }()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedString("statsScreen.emptyPlaceholder", comment: "")
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = UIColor(named: "YPBlack")
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let placeholderView: UIView = {
+        let placeholderView = UIView()
+        return placeholderView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "YPWhite")
+        
+        let placeholderSubviews = [placeholderImageView, placeholderLabel]
+        placeholderSubviews.forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            placeholderView.addSubview(item)
+        }
+        
+        let views = [titleLabel, placeholderView]
+        views.forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(item)
+        }
+        
+        setUpConstraints()
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -105),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 44),
+            
+            placeholderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            placeholderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            placeholderView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeholderView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            placeholderImageView.centerXAnchor.constraint(equalTo: placeholderView.centerXAnchor),
+            placeholderImageView.centerYAnchor.constraint(equalTo: placeholderView.centerYAnchor),
+            placeholderImageView.heightAnchor.constraint(equalToConstant: 80),
+            placeholderImageView.widthAnchor.constraint(equalToConstant: 80),
+            
+            placeholderLabel.topAnchor.constraint(equalTo: placeholderImageView.bottomAnchor, constant: 8),
+            placeholderLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            placeholderLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 }
